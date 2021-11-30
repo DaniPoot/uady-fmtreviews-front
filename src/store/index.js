2ab893bbbb370f0c38cd67a19af2dc87beaf67f0
DaniPoot <danielpoot.uc2@gmail.com>
2021-11-29 23:18:54 -0600

    Add courses service on store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore } from 'vuex'
 import * as ProfessorsApi from '@/services/ProfessorsApi'
+import * as CoursesApi from '@/services/CoursesApi'
 
 const store = createStore({
   modules: {}
 })
 
 store['$professorsApi'] = ProfessorsApi
+store['$coursesApi'] = CoursesApi
+
 export default store
