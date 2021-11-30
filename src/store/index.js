a614292a54cb16e2204dd1a1df5eb037a2133708
DaniPoot <danielpoot.uc2@gmail.com>
2021-11-29 22:27:16 -0600

    Add professor service on store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore } from 'vuex'
+import * as ProfessorsApi from '@/services/ProfessorsApi'
 
 const store = createStore({
   modules: {}
 })
 
+store['$professorsApi'] = ProfessorsApi
 export default store
