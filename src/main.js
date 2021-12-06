1aaeef235b6a46970c703f16aa445776699492da
DaniPoot <danielpoot.uc2@gmail.com>
2021-12-06 10:27:13 -0600

    Add new icon

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ import utils from './mixins/utils'
 import 'virtual:windi.css'
 import.meta.env.VITE_API_URL
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPhone, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPhone, faPlus, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 library.add(faPhone)
 library.add(faPlus)
+library.add(faArrowLeft)
 const app = createApp(App)
 app.use(store).use(router).mixin(utils).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
